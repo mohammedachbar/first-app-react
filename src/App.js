ad0dff6b804138f9c74ef9a84934d30f92b14d9b
mohammedachbar <achbarmohammed@gmail.com>
2020-08-29 00:47:46 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1> hello wwwworld </h1>
+       <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">zbbi</NavbarBrand>
+          </div>
+        </Navbar>
+    </div>
+  );
+}
+
+export default App;
